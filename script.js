cc21a04a9a123599a936703674ed7a26710a79e3
Estevão Henrique <113246534+Estevao15@users.noreply.github.com>
2022-11-21 09:59:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+ x = ('2022')
+var y = prompt('Digite o ano que nasceu.')
+var nome = prompt('Digite seu nome.')
+ idade = x-y
+
+let mensagem = `Olá, seu nome é ${nome} e você tem ${idade} anos.` 
+
+alert(mensagem)
